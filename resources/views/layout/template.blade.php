b3414419a4d4af1dd7a6ef8596cebf1ecc04a0ab
Christophe Ferru <christophe.ferru@gmail.com>
2021-11-29 06:57:21 -0500

    modif en tete

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Admin</title>
+        <title>[DEV 1.0] Administration | YvanDVoyages</title>
     </head>
     <body>
         Welcome to the admin panel. This is currently work in progress
